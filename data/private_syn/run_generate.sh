9fbd5ff5fc04de52b63b3c242655f4cbea4d4c24
Chen Gong <ChenG_abc@outlook.com>
2024-11-25 00:44:06 -0500

    exe filter

--- data/private_syn/run_generate.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python data/private_syn/generate.py \
-                                --model Qwen/Qwen2.5-Coder-1.5B \
-                                --ckpt /bigtemp/fzv6en/liuzheng/dpcode/checkpoints_code/magicoder/Qwen2.5-Coder-1.5B/dp10_lambda1000to0.1_alpha0.01_merged/checkpoint-15
