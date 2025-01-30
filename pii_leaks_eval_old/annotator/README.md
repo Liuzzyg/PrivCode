04e983e4e755f224e173c2035c14104dd047cda1
Chen Gong <ChenG_abc@outlook.com>
2025-01-30 12:37:52 -0500

    Reapply .gitignore

--- pii_leaks_eval_old/annotator/README.md
+++
@@ -1,6 +0,0 @@
-## Annotator for CodexLeaks Project
-1. Install rust with `rustup`.
-2. Setup `.env` file with proper `DATABASE_URL` variable (optional: github api token for github search api calls), run:
-```bash
-cargo run
-```
--- read_args.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-# 读取 .bin 文件
-file_path = "/bigtemp/fzv6en/liuzheng/dpcode/checkpoints_code/magicoder/Qwen2.5-Coder-1.5B/dp10_lambda1000to0.1_alpha0.01/checkpoint-2/training_args.bin"
-training_args = torch.load(file_path)
-
-# 查看内容
-print(training_args)
